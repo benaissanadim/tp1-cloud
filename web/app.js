cdd09057c078fb7743763c954b6fa3e30c603bc3
BEN AISSA Nadim <80357350+benaissanadim@users.noreply.github.com>
2023-09-12 16:30:34 +0200

    add

--- web/app.js
+++ web/app.js
@@ -7,7 +7,7 @@ const pool = new Pool({
   user: process.env.POSTGRESQL_ADDON_USER,
   host: process.env.POSTGRESQL_ADDON_HOST,
   database: process.env.POSTGRESQL_ADDON_DB,
-  password: process.env.POSTGRESQL_ADDON_DB,
+  password: process.env.POSTGRESQL_ADDON_PASSWORD,
   port: process.env.POSTGRESQL_ADDON_PORT
 });
 
